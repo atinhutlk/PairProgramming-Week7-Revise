4af3c8efa15bafa681da140e6045ffec05966f83
atinhutlk <nhut.vo@metropolia.fi>
2025-12-03 10:50:36 +0200

    Phase1-Step1

---
+++ week7-fepp-en/backend/routes/userRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userControllers");
+
+const router = express.Router();
+
+router.get("/", getAllUsers);
+router.post("/", createUser);
+router.get("/:userId", getUserById);
+router.put("/:userId", updateUser);
+router.delete("/:userId", deleteUser);
+module.exports = router;
